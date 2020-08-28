a49e4a3f0e3804c59a5ec5a204643d889ab5f236
zenglingji <zenglingji@yy.com>
2020-08-28 19:27:30 +0800

    feat: 添加主页组件

---
+++ components/home/index.tsx
@@ -0,0 +1,56 @@
+import styles from './index.module.css'
+
+export default function Homepage() {
+  return (
+    <div className={styles['root']}>
+      <header className={styles['coma-header']}>
+        <h1>Coma's Cyber Space</h1>
+      </header>
+      <main className={styles['coma-main']}>
+        <p className={styles['coma-paragrapj_p']}>身体里的碳，可以制成九千支铅笔，赠给诗人，但每根铅笔必须配一块橡皮</p>
+        <p className={styles['coma-paragrapj_p']}>身体里的磷，可以制成两千根火柴，全部给盲者，让他们点燃血中的火焰</p>
+        <p className={styles['coma-paragrapj_p']}>身体的脂肪，还能制成八块肥皂，赠给妓女，送给她洗净骨头去做母亲</p>
+        <p className={styles['coma-paragrapj_p']}>身体里的铁，只够打一枚钢钉，留给我漂泊一世的灵魂，就钉在爱人的心上</p>
+        <p className={styles['coma-paragrapj_p']}>要我说，我们身体里的10%的氢元素，来自于这个宇宙洪荒时的巨响</p>
+        <p className={styles['coma-paragrapj_p']}>还有18%的碳、65%的氧和3%的氮，那是恒星的晚年遗产</p>
+        <p className={styles['coma-paragrapj_p']}>还有那1.5%的钙，1%的磷</p>
+        <p className={styles['coma-paragrapj_p']}>以及那不够1%的钾、钠、镁、硫、氯</p>
+        <p className={styles['coma-paragrapj_p']}>这是巨人的怒吼，是巨星的狂风</p>
+        <p className={styles['coma-paragrapj_p']}>最后再赠与我们每人一根铁钉</p>
+        <p className={styles['coma-paragrapj_p']}>当然也少不了盘古的恩赐</p>
+        <p className={styles['coma-paragrapj_p']}>那些微乎其微的超铁元素</p>
+        <p className={styles['coma-paragrapj_p']}>是宇宙中最壮美的注脚</p>
+        <p className={styles['coma-paragrapj_p']}>我们身体里的每一个原子</p>
+        <p className={styles['coma-paragrapj_p']}>都曾经参与了这个世界中最宏大的故事</p>
+        <p className={styles['coma-paragrapj_p']}>是的 我们都是星辰之子</p>
+        <p className={styles['coma-paragrapj_p']}>不知道当面对宇宙与自身联结的事实之时</p>
+        <p className={styles['coma-paragrapj_p']}>我们会不会对短暂的生命</p>
+        <p className={styles['coma-paragrapj_p']}>少了那么一丝遗憾</p>
+        <p className={styles['coma-paragrapj_p']}>我相信，一定会的</p>
+        <p className={styles['coma-paragrapj_p']}>但我们知道，这是一种奇怪的心理</p>
+        <p className={styles['coma-paragrapj_p']}>宇宙爆炸成尘，我们也都来自于那不知何处的星尘</p>
+        <p className={styles['coma-paragrapj_p']}>但谁又能对回归尘埃，抱以充分的豁达呢</p>
+        <p className={styles['coma-paragrapj_p']}>但是这一切终究是注定的结局</p>
+        <p className={styles['coma-paragrapj_p']}>谁也无力挽回</p>
+        <p className={styles['coma-paragrapj_p']}>所以，不如就让我们以一种超脱的眼光来看待这一切吧</p>
+        <p className={styles['coma-paragrapj_p']}>如果末日到了，许多人心该会惶惶</p>
+        <p className={styles['coma-paragrapj_p']}>只怕再也无缘于这人间的俯仰，来领略这将逝的苍茫</p>
+        <p className={styles['coma-paragrapj_p']}>可凡身如果离了，其实它依旧无恙</p>
+        <p className={styles['coma-paragrapj_p']}>只是分做了千千万万的能量，共时空流淌</p>
+        <p className={styles['coma-paragrapj_p']}>比如先辈们的热血，就是届时将会喷薄的岩浆</p>
+        <p className={styles['coma-paragrapj_p']}>而我不劳恐慌，我知道宇宙所想，也该与人儿一样</p>
+        <p className={styles['coma-paragrapj_p']}>所以为了永生，只有幻灭众生的相</p>
+        <p className={styles['coma-paragrapj_p']}>可我因此而有了些许的失望，只可惜未来的生命，看不到我相貌得堂堂</p>
+        <p className={styles['coma-paragrapj_p']}>而人的翅膀，其实早在意识里潜藏</p>
+        <p className={styles['coma-paragrapj_p']}>只为在生命的终场，成就最壮美的飞翔</p>
+        <p className={styles['coma-paragrapj_p']}>起，灵魂为舱</p>
+        <p className={styles['coma-paragrapj_p']}>只要你想飞到天上，只要你想超越死亡</p>
+        <p className={styles['coma-paragrapj_p']}>但我若不想，不是我惧怕死亡</p>
+        <p className={styles['coma-paragrapj_p']}>只是趴在陌生的天上，我的爱人不见了我，会老在永恒的悲伤</p>
+      </main>
+      <footer className={styles['coma-footer']}>
+        <a href="//www.beian.miit.gov.cn">粤ICP备17070746号</a>
+      </footer>
+    </div>
+  )
+}
